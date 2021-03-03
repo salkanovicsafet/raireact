1cb00f0b48ee7671cd887f5e806d34c8602346bc
Safet Salkanović <salkanovicsafet@gmail.com>
2021-03-03 10:10:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Ocene from "./Ocene";
+
+function App() {
+
+
+
+  return (
+    <div>
+      <h1>Selam</h1>
+      <p>Dobro i?</p>
+      <code>hehe</code>
+      <Ocene ime="Damess Maafaka"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Ocene.js
@@ -0,0 +1,17 @@
+function Ocene(props) {
+    return(
+        <div>
+            <h3>
+                {props.ime}
+            </h3>
+            <h2>
+                Ime predmeta
+            </h2>
+            <h1>
+                Ocena
+            </h1>
+        </div>
+    );
+}
+
+export default Ocene;
